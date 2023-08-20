41c5e3ca0a39eef25a788f2866f66df66c458a7e
Sergio Stanly <74929911@certus.edu.pe>
2023-08-19 21:50:00 -0500

    cambio navbar

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const btn = document.getElementById("btn");
+const btnMenu = document.getElementById("btnMenu");
+
 let i = 0;
 const rootStyle = document.documentElement.style;
 btn.addEventListener("click",()=>{
@@ -7,14 +9,16 @@ btn.addEventListener("click",()=>{
         rootStyle.setProperty("--color-text","white");
         rootStyle.setProperty("--color-button-shadow","1px -1px 43px 0px rgba(17,117,254,0.75)");
         rootStyle.setProperty("--color-shadow","rgba(0,108,255,0.75)");
-        rootStyle.setProperty("--background","black");
+        rootStyle.setProperty("--background-todo","black");
+        rootStyle.setProperty("--color-navbar","rgb(122, 122, 122)");
         i=1;
     }else{
         btn.textContent="toggle_on";
         rootStyle.setProperty("--color-text","black");
         rootStyle.setProperty("--color-button-shadow","1px -1px 20px -4px rgba(255, 43, 185, 0.99)");
         rootStyle.setProperty("--color-shadow","rgba(255, 43, 185, 0.99)");
-        rootStyle.setProperty("--background","white");
+        rootStyle.setProperty("--background-todo","rgb(237, 237, 237)");
+        rootStyle.setProperty("--color-navbar","rgb(231, 231, 231)");
         i=0;
     }
 })
